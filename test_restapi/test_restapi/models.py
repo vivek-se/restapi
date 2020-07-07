b75a9757a086fbf787ff972d7b0ca9dd0dd00e31
Vivek Malkapuram <43130743+vivek-se@users.noreply.github.com>
2020-07-07 21:41:08 +0530

    updated with signals

--- test_restapi/test_restapi/models.py
+++ test_restapi/test_restapi/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 
 class student(models.Model):
 	student_id = models.IntegerField(primary_key=True)
@@ -11,4 +12,13 @@ class library(models.Model):
 	book_name = models.CharField(max_length=100)
 	student_id = models.ForeignKey(student, on_delete=models.CASCADE)
 
-	
+
+def student_save(sender, instance, **kwargs):
+	 print("New student saved")
+	 sobject = student.objects.latest('student_id')
+	 bid = getattr(sobject, 'book_id')
+	 obj=library.objects.create(book_id = bid, student_id = sobject)
+	 obj.save()
+
+#for signal
+post_save.connect(student_save, sender = student)
